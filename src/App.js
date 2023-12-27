b9995b2782a631a02c3c2587a2cc56d45e87bb09
ash1198 <ash.ranjan09@gmail.com>
2023-12-28 01:20:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import styles from "./styles.module.scss";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+    axios.defaults.baseURL = "http://localhost:1198/api/v1";
+    const [message, setMessage] = useState("message");
+    const [dateTime, setDateTime] = useState(new Date().toISOString());
+
+    const handleSetMessage = async () => {
+        try {
+            const config = {
+                type: "GET",
+                url: "/a2rp",
+            };
+            const response = await axios(config);
+            console.log(response);
+            const data = response.data;
+            if (data.success === true) {
+                setMessage(data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+            setMessage(error.message);
+        }
+    };
+    useEffect(() => {
+        handleSetMessage();
+    }, []);
+
+    const handleSetDateTime = () => {
+        const value = new Date().toISOString();
+        setDateTime(value);
+    };
+    useEffect(() => {
+        const timeout = setTimeout(handleSetDateTime, 1000 * 1 / 30);
+        return () => { clearTimeout(timeout); };
+    }, [dateTime]);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.messageFromAPI}>Message from backend &rArr; {message}</div>
+            <div className={styles.dateTime}>Date time &rArr; {dateTime}</div>
+
+            <ToastContainer />
+        </div>
+    );
+}
+
+export default App;
